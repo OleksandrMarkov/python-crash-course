cbbdc7433c5e46529398428a015afe439a183247
Oleksandr Markov <ol.markov1999@gmail.com>
2021-10-02 20:28:07 +0300

    4

---
+++ 4_work_with_lists/4.py
@@ -0,0 +1,30 @@
+# 4.1
+
+pizza_lst = ['p1', 'p2', 'p3']
+
+for p in pizza_lst:
+	print(p)
+
+for p in pizza_lst:
+	print(f"I like {p} pizza")
+print("I really love pizza!")
+
+# 4.2
+
+animals	= ['dog', 'horse', 'pig']
+
+for a in animals:
+	print(f"The {a}s are useful for humanity")
+print("Any of them are useful for humanity")
+
+
+# 4.3
+
+# 4.31
+#for i in range(1,21):
+#	print(i)
+
+arr = [i for i in range(1,21)]
+print(arr)
+
+print(i for i in range(1,21))
