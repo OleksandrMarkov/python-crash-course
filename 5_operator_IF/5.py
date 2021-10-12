ebff1d709c52d925fc75a2e07301395881fb30ed
Oleksandr Markov <ol.markov1999@gmail.com>
2021-10-12 19:51:34 +0300

    5.1

---
+++ 5_operator_IF/5.py
@@ -0,0 +1,36 @@
+#5.1
+car = 'audi'
+food = 'cake'
+id = 25
+result = 200
+music = 'rock'
+
+print('Is car == "BMW"? I predict FALSE')
+print(car == "BMW")
+print()
+print('Is car == "audi"? I predict TRUE')
+print(car == "audi")
+print()
+print('Is food == "vegetables"? I predict FALSE')
+print(car == "vegetables")
+print()
+print('Is food == "cake"? I predict TRUE')
+print(food == "cake")
+print()
+print('Is id == "34"? I predict FALSE')
+print(id == "34")
+print()
+print('Is id == "25"? I predict TRUE')
+print(id == 25)
+print()
+print('Is result == "200"? I predict FALSE')
+print(result == "200")
+print()
+print('Is result == 200? I predict TRUE')
+print(result == 200)
+print()
+print('Is music == "jazz"? I predict FALSE')
+print(music == "jazz")
+print()
+print('Is music == "rock"? I predict TRUE')
+print(result == 'rock')
