569db5c06e373f7331e26902d0379553930bb2cc
Oleksandr Markov <ol.markov1999@gmail.com>
2021-10-14 11:57:32 +0300

    7.4-.7.7

--- 7_user_input_and_WHILE/7.py
+++ 7_user_input_and_WHILE/7.py
@@ -7,14 +7,12 @@ print('# introduction')
 
 print()
 print('#7.1')
-print()
 
 #msg = input("What auto would you like to rent? ")
 #print(f"I will try to find {msg} for you.")
 
 print()
 print('#7.2')
-print()
 
 #tables = input("How many tables would you like to book? ")
 #tables = int(tables)
@@ -26,12 +24,61 @@ print()
 
 print()
 print('#7.3')
+
+#num = input("Number: ")
+#num = int(num)
+
+#if num % 10 == 0:
+#	print(f"{num} is multiple to ten.")
+#else:
+#	print(f"{num} is not multiple to ten.")
+
+
+print()
+print('#7.4')
+
+#ingredient = ""
+#while ingredient != 'quit':
+#	ingredient = input("Ingredient (enter 'quit' when you are finished): ")
+#	if ingredient != 'quit':
+#		print(f"{ingredient} added to our pizza!")
+
+
+print()
+print('#7.5')
+
+#while True:
+#	age = input("How old are you? ")
+#	age = int(age)
+
+#	if age < 3:
+#		print(age)
+#		print("The entrance is free")
+#	elif 3 <= age <= 12:
+#		ticket_price = 10
+#		print(age) 
+#		print(f"The ticket costs {ticket_price}$.")
+#	else:
+#		ticket_price = 15
+#		print(age) 
+#		print(f"The ticket costs {ticket_price}$.")	
+#	break
+
+print('#7.6')
 print()
 
-num = input("Number: ")
-num = int(num)
+#active = True
+#ingredient = ""
+#while active:
+#	ingredient = input("Ingredient (enter 'quit' when you are finished): ")
+#	if ingredient == 'quit':
+#		break
+#	else:
+#		print(f"{ingredient} added to our pizza!")
 
-if num % 10 == 0:
-	print(f"{num} is multiple to ten.")
-else:
-	print(f"{num} is not multiple to ten.")
+
+print('#7.7')
+print('eternal cycle')
+#x = 0
+#while x < 1:
+#	print(x)
