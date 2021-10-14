15b0d78d1c3d000df2e5a0ff2f3df451ea780a53
Oleksandr Markov <ol.markov1999@gmail.com>
2021-10-14 11:06:48 +0300

    7.1-7.3

---
+++ 7_user_input_and_WHILE/7.py
@@ -0,0 +1,37 @@
+# introduction
+
+print('# introduction')
+
+#msg = input("tell me sth: ")
+#print(msg)
+
+print()
+print('#7.1')
+print()
+
+#msg = input("What auto would you like to rent? ")
+#print(f"I will try to find {msg} for you.")
+
+print()
+print('#7.2')
+print()
+
+#tables = input("How many tables would you like to book? ")
+#tables = int(tables)
+
+#if tables > 8:
+#	print("You will have to wait")
+#else:
+#	print("Your tables are ready")
+
+print()
+print('#7.3')
+print()
+
+num = input("Number: ")
+num = int(num)
+
+if num % 10 == 0:
+	print(f"{num} is multiple to ten.")
+else:
+	print(f"{num} is not multiple to ten.")
