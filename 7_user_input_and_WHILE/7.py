cc2161786beca0511d5c33661ae6c2828bba1080
Oleksandr Markov <ol.markov1999@gmail.com>
2021-10-14 12:25:02 +0300

    7.8-7.10

--- 7_user_input_and_WHILE/7.py
+++ 7_user_input_and_WHILE/7.py
@@ -82,3 +82,64 @@ print('eternal cycle')
 #x = 0
 #while x < 1:
 #	print(x)
+
+#removing all cats using cycle
+
+#pets = ['cat', 'dog', 'swine', 'dog', 'cat', 'rabbit', 'rabbit', 'cat', 'mouse']
+
+#while 'cat' in pets:
+#	pets.remove('cat')
+#print(pets)
+
+print()
+print('#7.8')
+sandwich_orders = ['s1', 's2', 's3', 's4', 's5']
+finished_sandwiches = []
+
+#while sandwich_orders:
+#	tmp_order = sandwich_orders.pop()
+#	print(f"I made you {tmp_order} sandwich!")
+#	finished_sandwiches.append(tmp_order)
+#print("Sandwiches are ready:")
+#for fs in finished_sandwiches:
+#	print(f"\t{fs}")
+
+print()
+print('#7.9')
+sandwich_orders = ['s1', 'pastrami', 's2', 'pastrami', 's3', 'pastrami', 
+'s4', 'pastrami', 'pastrami', 's5', 'pastrami']
+print('We are out of pastrami')
+
+while 'pastrami' in sandwich_orders:
+	sandwich_orders.remove('pastrami')
+
+while sandwich_orders:
+	tmp_order = sandwich_orders.pop()
+	print(f"I made you {tmp_order} sandwich!")
+	finished_sandwiches.append(tmp_order)
+
+print("Sandwiches are ready:")
+for fs in finished_sandwiches:
+	print(f"\t{fs}")	
+
+
+print()
+print('#7.10')
+
+polling_active = True
+responses = {}
+
+while polling_active:
+	name = input("\nWhat is your name? ")
+	place = input("\nWhere would you like to visit? ")
+	responses[name] = place
+
+	repeat = input('\nWould you like to let another person respond? (yes/no) ')
+
+	if repeat == 'no':
+		polling_active = False
+
+print("\n --- Poll results ---")
+
+for name, place in responses.items():
+	print(f"{name} would like to visit {place}")
