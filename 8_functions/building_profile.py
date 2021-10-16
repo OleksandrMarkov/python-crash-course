239223a3c318ec6ff77683f5562a9cdeaf776e32
Oleksandr Markov <ol.markov1999@gmail.com>
2021-10-16 22:22:35 +0300

    building of my profile as 8.16

---
+++ 8_functions/building_profile.py
@@ -0,0 +1,4 @@
+def build_profile(first, last, **my_info):
+	my_info['first'] = first
+	my_info['last'] = last
+	return my_info
---
+++ 8_functions/my_profile.py
@@ -0,0 +1,21 @@
+#var 1
+#from building_profile import *
+#my_profile = build_profile('alex', 'markov', age = 22, height = 175)
+
+#var 2
+#from building_profile import build_profile
+#my_profile = build_profile('alex', 'markov', age = 22, height = 175)
+
+#var 3
+#from building_profile import build_profile as bp
+#my_profile = bp('alex', 'markov', age = 22, height = 175)
+
+#var 4
+#import building_profile
+#my_profile = building_profile.build_profile('alex', 'markov', age = 22, height = 175)
+
+#var 5
+#import building_profile as bp
+#my_profile = bp.build_profile('alex', 'markov', age = 22, height = 175)
+
+print(my_profile)
