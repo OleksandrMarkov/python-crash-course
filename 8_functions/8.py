f419a63c5431c706f496b411dcdcc24deb5af160
Oleksandr Markov <ol.markov1999@gmail.com>
2021-10-16 17:43:42 +0300

    8.1-8.2

---
+++ 8_functions/8.py
@@ -0,0 +1,18 @@
+"""it is docstring"""
+
+def	greet_user(username):
+	""" Show simple message"""
+	print(f"Hello, {username.title()}!")
+
+
+def	task81():
+	""" info about the 8th chapter"""
+	print(f"some info")
+
+def favorite_book(book):
+	"""task #8.2: my favorite book"""
+	print(f"One of my favorite books is {book.title()}") 
+
+greet_user('Alex')
+task81()
+favorite_book("bible")	
