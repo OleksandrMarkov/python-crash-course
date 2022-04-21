f53cc74dbf8107a2cbbe6f19a284272ea501b925
oleksandr markov <ol.markov1999@gmail.com>
2022-04-21 12:23:26 +0300

    Class to save all game settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,17 +2,20 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
 	"""Class that manages the resources and behavior of the game"""
 	
 	"""Game initialization, resource creation"""
 	def __init__(self):
 		pygame.init()
+		self.settings = Settings()
 
-		self.screen = pygame.display.set_mode((1200, 800))
+		self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+		
 		pygame.display.set_caption("Alien Invasion")
 		self.bg_color = (230,230,230) # set background color
-
 
 	def run_game(self):
 		""" Start main game loop"""
@@ -22,11 +25,11 @@ class AlienInvasion:
 				if event.type == pygame.QUIT:
 					sys.exit()
 				""" redraw the screen again on every iteration """		
-				self.screen.fill(self.bg_color)			
+				self.screen.fill(self.settings.bg_color)			
 			"""show last drawn screen"""		
 			pygame.display.flip()
 
 if __name__ == '__main__':
 	# create an instance and run the game
 	ai = AlienInvasion()
-	ai.run_game()							
+	ai.run_game()						
---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+	"""Class to save all game settings"""
+	def __init__(self):
+		"""initialize game settings """
+		# Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230, 230, 230)
